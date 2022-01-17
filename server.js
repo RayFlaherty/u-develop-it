6c622fda03a9b93889285b20f1646b485b5d3749
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-01-16 18:37:59 -0600

    debugging

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
-const db = require('./db/connection');
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
-// Add near the top of the file
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World'
-  });
-});
-// Add after Express middleware
-app.use('/api', apiRoutes);
-
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
-
-// module.exports = db;
